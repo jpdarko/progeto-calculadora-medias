154df30603b32386d5cff784ffa1ef2acc9703df
Your Name <jpaulo11x@gmail.com>
2023-07-24 20:53:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionarLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionarLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+    } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
+
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${inputNomeAtividade.value}</td>
+            <td>${inputNotaAtividade.value}</td>
+            <td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>
+        `;
+
+        document.querySelector('tbody').appendChild(linha);
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = '';
+    atividades.forEach((atividade, index) => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${atividade}</td>
+            <td>${notas[index]}</td>
+            <td>${notas[index] >= notaMinima ? imgAprovado : imgReprovado}</td>
+        `;
+        corpoTabela.appendChild(linha);
+    });
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
+}
